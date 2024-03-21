d560b0c4b8fe9e4654a98d0ca0cefe9c6c10fa5a
pschl1337 <patrick.scheel@outlook.com>
2024-03-21 10:34:01 +0000

    ASDF

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -115,8 +115,7 @@ except ImportError:
     logger.info("Using default Docker config...")
 
 SECRET_KEY = 'C1E65bWGVJXiLjo6lBe816LDYVNbP06e1kdBxjKdCoM='
-FILTER_STATE_CACHE_CONFIG = |
-  {
+FILTER_STATE_CACHE_CONFIG = {
     'CACHE_TYPE': 'redis',
     'CACHE_KEY_PREFIX': 'superset_results',
     'CACHE_REDIS_URL': 'redis://localhost:6379/0'
